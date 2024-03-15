91b28adecd42fd6aca6584d525ed5a3c1331eb21
ChhunSokvisothh <156287525+ChhunSokvisothh@users.noreply.github.com>
2024-03-15 21:59:30 +0700

    Revert "First draft of code"
    This reverts commit ccdf7a7f8f6fde054a75b9840f497197cdf73ea3.

--- exercise-1.py
+++ exercise-1.py
@@ -1,11 +1,2 @@
 def replace_last(numbers):
-    replace_last = numbers[-1] + numbers [:-1]
-    return replace_last
-
-def main():
-    numbers = float(input("Enter you list of number").split())
-    print(f"Here is your new list with the last number in the front:",{replace_last(numbers)})
-
-    
-
-
+    ...

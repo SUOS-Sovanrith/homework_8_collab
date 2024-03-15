a01568ccaf4ac86fdcfcccdb72511cf3705562ea
ChhunSokvisothh <156287525+ChhunSokvisothh@users.noreply.github.com>
2024-03-15 21:55:14 +0700

    Final commit

--- exercise-1.py
+++ exercise-1.py
@@ -1,11 +1,17 @@
 def replace_last(numbers):
-    replace_last = numbers[-1] + numbers [:-1]
-    return replace_last
+    if len(numbers) == 0 and len(numbers) == 1:
+        return numbers
+    else:
+        replace_last = numbers[-1:] + numbers [:-1]
+        return replace_last
 
 def main():
-    numbers = float(input("Enter you list of number").split())
-    print(f"Here is your new list with the last number in the front:",{replace_last(numbers)})
+    numbers = input("Enter you list of number:").split()
 
-    
+    print(f"Here is your list with the last digit in the front:",replace_last(numbers))
+
+
+if __name__ == "__main__":
+    main()
 
 

593ab499db87db46a68f225e42366d18cf4e1b1f
HyLyheang <2024062hy@aupp.edu.kh>
2024-03-16 21:00:21 +0700

    Update exercise-5.py
    first_try

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,10 @@
+numbers = [1,2,3,4,5]
+
 def reverse_ascending(numbers):
-    ...
+    numbers.sort()
+    numbers.reverse()
+    new_numbers = numbers
+    return new_numbers
+
+print(reverse_ascending(numbers))
+
